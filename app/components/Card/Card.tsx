4f34aab2b0a416bc364fdc5e421c4cc8991fc78e
christopher coyne <christcoyne@gmail.com>
2023-10-24 20:49:30 -0400

    added card component

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,10 +1,11 @@
 import styles from './Card.module.css';
+import {HydroLink} from '../Link/Link';
 export const Card = ({title, description, link}: any) => {
   return (
     <div className={styles.card}>
       <h3>{title}</h3>
       <p>{description}</p>
-      <p>{link}</p>
+      <HydroLink to={link.destination} title={title} />
     </div>
   );
 };
